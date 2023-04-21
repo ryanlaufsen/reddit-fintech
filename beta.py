8601d542744d7f89d893f67b1cd88e7ca71cdcbd
car-feng <98931752+car-feng@users.noreply.github.com>
2023-04-21 11:41:53 +0800

    Update beta.py
    added column to retrieve the stock mentioned in the column, however the stock mentioned must be in all caps pattern

--- beta.py
+++ beta.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import praw
+import re
 
 reddit = praw.Reddit(client_id='QzpLEbJDxX2PYJb9_43duw',
                      client_secret='31K5aLH4-nIwYitmIdFzIXqRW57LXw',
@@ -22,9 +23,17 @@ for submission in reddit.subreddit('wallstreetbets').hot(limit=1):
         author.append(comment.author)
         comments.append(comment.body)
 
+# extract stock mentions from comments
+stocks = ['AAPL', 'GOOGL', 'TSLA', 'AMZN']  # list of stock symbols/names
+pattern = '|'.join(stocks)  # regex pattern for matching stock mentions
+mentions = []
+for comment in comments:
+    mention = re.findall(pattern, comment, re.IGNORECASE)  # find stock mentions using regex
+    mentions.append(mention)
+
 dict_of_dfs = {}
 for i in range(len(titles)):
-    df_user_comment = pd.DataFrame({'user_id': author, 'comment': comments})
+    df_user_comment = pd.DataFrame({'user_id': author, 'comment': comments, 'stock_mention': mentions})
     dict_of_dfs[titles[i]] = df_user_comment
 
 df = pd.concat(dict_of_dfs, axis=1)
