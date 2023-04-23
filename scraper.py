488e051bcab86eee892f61e842f4ec8f41dfbe8c
Ryan <ryan.y@shaw.ca>
2023-04-23 07:09:15 -0700

    Use PRAW to get data from Reddit

---
+++ scraper.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+import praw
+from datetime import datetime
+import csv
+
+load_dotenv()
+
+reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
+                     client_secret=os.environ['REDDIT_CLIENT_SECRET'],
+                     user_agent='stock_sentimental_analysis')
+
+
+subreddit = reddit.subreddit('wallstreetbets')
+
+start_date = datetime(2023, 2, 27)
+end_date = datetime(2023, 3, 31)
+
+titles = []
+authors = []
+comments = []
+
+for submission in subreddit.search('flair_name:"Daily Discussion"', limit=None):
+    if start_date <= datetime.fromtimestamp(submission.created_utc) <= end_date:
+        if "What Are Your Moves" in submission.title:
+            for comment in submission.comments:
+                if isinstance(comment, praw.models.MoreComments):
+                    continue
+
+                titles.append(submission.title)
+                authors.append(comment.author)
+                comments.append(comment.body)
+                
+# Write data to CSV file
+with open('daily_discussion_moves.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Title', 'Author', 'Comment'])
+    for i in range(len(comments)):
+        writer.writerow([titles[i], authors[i], comments[i]])
+
+
+
+
+
