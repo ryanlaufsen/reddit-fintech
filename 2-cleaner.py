a4abfb7ae0f7ce189207643b4926d7dc8a43c2b2
John Smith <carson.feng@mail.utoronto.ca>
2023-04-23 23:48:56 +0800

    import dataframe

--- 2-cleaner.py
+++ 2-cleaner.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data/daily_discussion_moves.csv')
+df = pd.read_csv('daily_discussion_moves.csv')
 
 # Convert post title into dates
 df['Title'] = pd.to_datetime(df['Title'].str[30:])
