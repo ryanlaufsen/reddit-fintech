dfd4e5d0e5640d3f351e026463aa0d86f99f949c
John Smith <carson.feng@mail.utoronto.ca>
2023-04-24 03:52:05 +0800

    Returned a dataframe with cleaned comment, and the sentiment score based on the cleaned comment. Comments are group by date.

--- 2-cleaner.py
+++ 2-cleaner.py
@@ -11,7 +11,12 @@ from praw.models import MoreComments
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk import FreqDist
+import datetime as dt
+import string
+from textblob import TextBlob
 
+
+nltk.download('stopwords')
 nltk.download('wordnet')
 
 df = pd.read_csv('daily_discussion_moves.csv')
@@ -19,68 +24,110 @@ df = pd.read_csv('daily_discussion_moves.csv')
 # Convert post title into dates
 df['Date'] = pd.to_datetime(df['Title'].str[30:])
 
-grouped_comments = df.groupby('Date')
+df = df.sort_values(by='Date', ascending=False)
 
-grouped_comments['Comment'].apply(lambda x: ' '.join(x))
+#df.to_csv('sample.csv', index=False)
 
-print
 
-dirty_comment = []
+grouped_comments_df = df.groupby(by='Date').apply(lambda x:x)
+grouped_comments_df = grouped_comments_df.drop('Date',axis = 1)
+
+
+def preprocess_text(text):
+    # Remove emoji and gif
+    text = re.sub(r'\\[a-zA-Z0-9]+', '', text)
+    text = re.sub(r'\[\w+\]\(emote\|t5_\w+\|\d+\)', '', text)
+    text = re.sub(r'\[\w+\]\(emote\|free_emotes_pack\|\w+\)', '', text)
+
+    # Tokenize and clean string
+    tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
+    tokenized_string = tokenizer.tokenize(text)
+
+    # Convert tokens to lowercase
+    lower_string_tokenized = [word.lower() for word in tokenized_string]
+
+    # Remove stopwords
+    all_stopwords = stopwords.words('english')
+    tokens_without_sw = [word for word in lower_string_tokenized if not word in all_stopwords]
+
+    # Lemmatize words
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = ([lemmatizer.lemmatize(w) for w in tokens_without_sw])
+
+    # Join cleaned tokens back into string
+    cleaned_output = ' '.join(lemmatized_tokens)
+
+    return cleaned_output
+
+#Update the comment column with cleaned comment
+grouped_comments_df['Cleaned Comment'] = grouped_comments_df['Comment'].apply(lambda x: preprocess_text(x))
+#print((grouped_comments_df))
+
+#Define a function to perform sentiment analysis using TextBlob
+def get_sentiment(text):
+    blob = TextBlob(text)
+    return blob.sentiment.polarity
+
+# Apply sentiment analysis to 'Comment' column
+grouped_comments_df['Sentiment_Score'] = grouped_comments_df['Cleaned Comment'].apply(lambda x: get_sentiment(x))
+print(grouped_comments_df)
+#dirty_comment = []
 
 # append all the comments into a list
-for comment in df['Comment']:
-    dirty_comment.append(comment)
+#for comment in df['Comment']:
+    #dirty_comment.append(comment)
 
 # print(dirty_comment)
 # preprocessing string
-list1 = dirty_comment
-list1 = [str(i) for i in list1]
-string_uncleand = ','.join(list1)
+#list1 = dirty_comment
+#list1 = [str(i) for i in list1]
+#string_uncleand = ','.join(list1)
 
 
 # print(string_uncleand)
 
 # remove emoji,gif
 
-def remove_emojis(text):
-    return demoji.replace(text, '')
+#def remove_emojis(text):
+    #return demoji.replace(text, '')
 
 
-emojiless = re.sub(r'\\[a-zA-Z0-9]+', '', string_uncleand)
-emojiless = re.sub(r'\[\w+\]\(emote\|t5_\w+\|\d+\)', '', emojiless)
-emojiless = re.sub(r'\[\w+\]\(emote\|free_emotes_pack\|\w+\)', '', emojiless)
+#emojiless = re.sub(r'\\[a-zA-Z0-9]+', '', string_uncleand)
+#emojiless = re.sub(r'\[\w+\]\(emote\|t5_\w+\|\d+\)', '', emojiless)
+#emojiless = re.sub(r'\[\w+\]\(emote\|free_emotes_pack\|\w+\)', '', emojiless)
 
 #print(emojiless)
 
 
 
 # tokenizing and cleaning strings
-tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
-tokenized_string = tokenizer.tokenize(emojiless)
+#tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
+#tokenized_string = tokenizer.tokenize(emojiless)
 #print(tokenized_string)
 
 #preproccing string - converting tokens into lowercase
-lower_string_tokenized = [word.lower() for word in tokenized_string]
+#lower_string_tokenized = [word.lower() for word in tokenized_string]
 #print(lower_string_tokenized)
 
 #removing stopwords
-nlp = en_core_web_sm.load()
+#nlp = en_core_web_sm.load()
 
-all_stopwords = nlp.Defaults.stop_words
+#all_stopwords = nlp.Defaults.stop_words
 
-text = lower_string_tokenized
-tokens_without_sw = [word for word in text if not word in all_stopwords]
+#text = lower_string_tokenized
+#tokens_without_sw = [word for word in text if not word in all_stopwords]
 #print(tokens_without_sw)
 
 #normalizing words via lemmatizing
 
-lemmatizer = WordNetLemmatizer()
+#lemmatizer = WordNetLemmatizer()
 
-lemmatized_tokens = ([lemmatizer.lemmatize(w) for w in tokens_without_sw])
+#lemmatized_tokens = ([lemmatizer.lemmatize(w) for w in tokens_without_sw])
 
 
 #store lemmatized words into a new variable
-cleand_output = lemmatized_tokens
+#cleand_output = lemmatized_tokens
+#print(cleand_output)
 
 #print('Original length of words = ' , (len(string_uncleand)))
 #print("num of words after removing emotes and gif = ",(len(emojiless)))
