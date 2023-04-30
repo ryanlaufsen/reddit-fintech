341eb99a2bb988a2aa51cd635b475f8074dbfe19
Ryan <ryan.y@shaw.ca>
2023-04-30 03:01:57 -0700

    Allow for indexed writing w/ pause / resume controls

---
+++ datawriter.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import importlib
+cleaner = importlib.import_module('2-cleaner')
+extractor = importlib.import_module('3-extractor')
+analyzer = importlib.import_module('4-analyzer')
+calc = importlib.import_module('5-calculator')
+
+def write(start_index, chunk_size):
+    df = pd.read_csv('data/daily_discussion_moves.csv',
+                    skiprows=None if start_index == 0 else range(1,start_index+1),
+                    nrows=chunk_size,
+                    on_bad_lines='warn')
+
+    # Extract stock tickers from raw comments
+    df['Ticker'] = df['Comment'].apply(
+        lambda x: extractor.get_tickers(x))
+    df = df.explode('Ticker')
+
+    # Calculate actual daily returns
+    df = df[df['Ticker'].notnull()]  # Remove rows with no ticker mentions
+    if df.empty:
+        return
+
+    # Convert post title into dates
+    df['Date'] = pd.to_datetime(df['Title'].str[30:])
+
+    df = df.sort_values(by='Date', ascending=True)
+
+    # Add cleaned comment column
+    df['Cleaned Comment'] = df['Comment'].apply(
+        lambda x: cleaner.preprocess_text(x))
+
+    # Apply sentiment analysis to cleaned comments
+    df['Sentiment Score'] = df['Cleaned Comment'].apply(
+        lambda x: analyzer.get_sentiment(x))
+
+    # Multiply sentiment by upvotes
+    df['Adjusted Sentiment Score'] = df.apply(
+        lambda x: x['Upvotes'] * x['Sentiment Score'], axis=1)
+
+    # Convert datetime to string
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+
+    df['Actual Return'] = df.apply(
+        lambda x: calc.get_next_day_return(x['Ticker'], x['Date']), axis=1)
+
+    df.to_csv(f'for_regression/for_regression_{start_index}_{start_index + chunk_size}.csv')
--- main.py
+++ main.py
@@ -1,46 +1,19 @@
 import pandas as pd
+import os
 import importlib
-cleaner = importlib.import_module('2-cleaner')
-extractor = importlib.import_module('3-extractor')
-analyzer = importlib.import_module('4-analyzer')
-calc = importlib.import_module('5-calculator')
+datawriter = importlib.import_module('datawriter')
 
-df = pd.read_csv('data/daily_discussion_moves.csv').sample(n=20)
+if not os.path.exists('data'):
+    os.makedirs('data')
+    print('Created /data directory.')
 
-# Extract stock tickers from raw comments
-df['Ticker'] = df['Comment'].apply(
-    lambda x: extractor.get_tickers(x))
-df = df.explode('Ticker')
+if not os.path.exists('for_regression'):
+    os.makedirs('for_regression')
+    print('Created /for_regression directory.')
 
-# Convert post title into dates
-df['Date'] = pd.to_datetime(df['Title'].str[30:])
+f = 'data/daily_discussion_moves.csv'
+starting_index = 0
+chunk_size = 100
 
-df = df.sort_values(by='Date', ascending=True)
-
-# Update the comment column with cleaned comment
-df['Cleaned Comment'] = df['Comment'].apply(
-    lambda x: cleaner.preprocess_text(x))
-
-# Apply sentiment analysis to 'Comment' column
-df['Sentiment Score'] = df['Cleaned Comment'].apply(
-    lambda x: analyzer.get_sentiment(x))
-
-# Multiply sentiment by upvotes
-df['Adjusted Sentiment Score'] = df.apply(
-    lambda x: x['Upvotes'] * x['Sentiment Score'], axis=1)
-
-# Calculate actual daily returns
-df = df[df['Ticker'].notnull()]  # Remove rows with no ticker mentions
-
-# Convert datetime to string
-df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-
-df['Actual Return'] = df.apply(
-    lambda x: calc.get_next_day_return(x['Ticker'], x['Date']), axis=1)
-
-# Sum sentiment, grouping by ticker and date
-df = df.groupby(['Ticker', 'Date']).agg({'Adjusted Sentiment Score': 'sum', 'Actual Return': 'mean'})
-
-print(df[['Ticker', 'Adjusted Sentiment Score', 'Actual Return', 'Date']])
-
-# df.to_csv('data/for_regression.csv')
+for i in range(starting_index, len(pd.read_csv(f)), chunk_size):
+    datawriter.write(i, chunk_size)
