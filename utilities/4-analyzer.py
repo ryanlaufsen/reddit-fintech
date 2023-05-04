b6fd027ee46a71d29251dad95a5a89e5fd33f6c5
Ryan <ryan.y@shaw.ca>
2023-05-04 03:22:17 -0700

    Replace TextBlob with SiEBERT

--- utilities/4-analyzer.py
+++ utilities/4-analyzer.py
@@ -1,14 +1,25 @@
-import pandas as pd
-import nltk
-from textblob import TextBlob
+import requests
+import os
+from dotenv import load_dotenv
 
-for resource in ['corpora/stopwords', 'corpora/wordnet']:
-    try:
-        nltk.find(resource)
-    except LookupError:
-        nltk.download(resource)
+load_dotenv()
 
-# Define a function to perform sentiment analysis using TextBlob
+model = 'siebert/sentiment-roberta-large-english'
+API_URL = f'https://api-inference.huggingface.co/models/{model}'
+headers = {"Authorization": f"Bearer {os.environ['HUGGINGFACE_TOKEN']}"}
+
+def analyze(data):
+    payload = dict(inputs=data, options=dict(wait_for_model=True))
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+# Perform sentiment analysis using SiEBERT
 def get_sentiment(text):
-    blob = TextBlob(text)
-    return blob.sentiment.polarity
+    try:
+        sentiment_result = analyze(text)[0]
+        top_sentiment = max(sentiment_result, key=lambda x: x['score']) # Get the sentiment with the higher score
+        polarity = 1 if top_sentiment['label'] == 'positive' else -1
+        return top_sentiment['score'] * polarity
+    
+    except Exception as e:
+        print(e)
